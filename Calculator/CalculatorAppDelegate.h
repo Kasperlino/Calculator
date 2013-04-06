b500c36974bc71a0afc489552325c93332e58d71
Erik Kasper <erikkasper@Eriks-MacBook-Pro.local>
2013-04-06 16:02:37 +0200

    Initial Commit

---
+++ Calculator/CalculatorAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CalculatorAppDelegate.h
+//  Calculator
+//
+//  Created by Erik Kasper on 06.04.13.
+//  Copyright (c) 2013 Erik Kasper. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CalculatorAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Calculator/CalculatorViewController.h
@@ -0,0 +1,13 @@
+//
+//  CalculatorViewController.h
+//  Calculator
+//
+//  Created by Erik Kasper on 06.04.13.
+//  Copyright (c) 2013 Erik Kasper. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CalculatorViewController : UIViewController
+
+@end
